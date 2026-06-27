548e59736370d27f3db6d80cf522afed998381e3
EC2 Default User <ec2-user@ip-172-31-36-168.eu-west-2.compute.internal>
2026-06-27 16:06:51 +0000

    adding CodeDeploy files

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo yum install tomcat -y
+sudo yum install httpd -y
+
+sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+<VirtualHost *:80>
+  ServerAdmin root@localhost
+  ServerName app.cicd.com
+  DefaultType text/html
+  ProxyRequests off
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:8080/cicd-web-project/
+  ProxyPassReverse / http://localhost:8080/cicd-web-project/
+</VirtualHost>
+EOF
